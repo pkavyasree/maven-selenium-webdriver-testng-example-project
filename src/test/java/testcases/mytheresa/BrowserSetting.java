dfa4d26589ef3994a344bbb086e2afc0e5defee3
pkavyasree <kavya.panchumarthy@gmail.com>
2020-06-07 07:44:38 -0700

    Update BrowserSetting.java

--- src/test/java/testcases/mytheresa/BrowserSetting.java
+++ src/test/java/testcases/mytheresa/BrowserSetting.java
@@ -16,7 +16,7 @@ public class BrowserSetting {
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		//driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
 		driver.get(BASE_URL);
 		return driver;
 	}
