82cef172239d2c113ff197f995455054a322cf77
pkavyasree <kavya.panchumarthy@gmail.com>
2020-06-07 07:25:56 -0700

    Update WebdriverSettings.java

--- src/test/java/testcases/mytheresa/WebdriverSettings.java
+++ src/test/java/testcases/mytheresa/WebdriverSettings.java
@@ -6,8 +6,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class WebdriverSettings {
 	
 	public WebDriver driverSettings() {
+		options = new ChromeOptions();
+                options.addArguments("disable-gpu");  
 		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		WebDriver driver = new ChromeDriver(options);
 		return driver;
 	}
 
