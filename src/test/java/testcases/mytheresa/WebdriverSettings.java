1e85242e8a4c18bcdbe6de015732da7f34a1aadf
pkavyasree <kavya.panchumarthy@gmail.com>
2020-06-09 18:03:22 +0530

    Update WebdriverSettings.java

--- src/test/java/testcases/mytheresa/WebdriverSettings.java
+++ src/test/java/testcases/mytheresa/WebdriverSettings.java
@@ -1,16 +1,15 @@
 package testcases.mytheresa;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class WebdriverSettings {
 	
 	public WebDriver driverSettings() {
 		System.setProperty("webdriver.gecko.driver","/usr/local/bin/geckodriver");
-	        driver=new FirefoxDriver();
-		driver.get("http://www.google.com");
+		WebDriver driver = new FirefoxDriver();
 		return driver;
+	       
 	}
 
 }
